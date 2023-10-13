d15b51809d37abec99c081889d319602425cbebc
Jean-Christian Denis <contact@jcdenis.fr>
2023-10-13 20:59:07 +0200

    cosmetic

--- _define.php
+++ _define.php
@@ -21,10 +21,10 @@ $this->registerModule(
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
-        'type'       => 'plugin',
-        'support'    => 'https://git.dotclear.watch/JcDenis/' . basename(__DIR__) . '/issues',
-        'details'    => 'https://git.dotclear.watch/JcDenis/' . basename(__DIR__) . '/src/branch/master/README.md',
-        'repository' => 'https://git.dotclear.watch/JcDenis/' . basename(__DIR__) . '/raw/branch/master/dcstore.xml',
+        'type'        => 'plugin',
+        'support'     => 'https://git.dotclear.watch/JcDenis/' . basename(__DIR__) . '/issues',
+        'details'     => 'https://git.dotclear.watch/JcDenis/' . basename(__DIR__) . '/src/branch/master/README.md',
+        'repository'  => 'https://git.dotclear.watch/JcDenis/' . basename(__DIR__) . '/raw/branch/master/dcstore.xml',
     ]
 );
 
