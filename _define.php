5097cc7736dae08d6c2599a56bb4afab9eb0529a
Jean-Christian Denis <contact@jcdenis.fr>
2023-10-13 20:57:41 +0200

    release 0.7

--- _define.php
+++ _define.php
@@ -1,27 +1,26 @@
 <?php
 /**
- * @brief mail2log, a plugin for Dotclear 2
+ * @file
+ * @brief       The plugin mail2log definition
+ * @ingroup     mail2log
  *
- * @package Dotclear
- * @subpackage Plugin
+ * @defgroup    mail2log Plugin mail2log.
  *
- * @author Jean-Christian Denis and contributors
+ * Do not send mails but log them.
  *
- * @copyright Jean-Christian Denis
- * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ * @author      Jean-Christian Denis (author)
+ * @copyright   GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-use Dotclear\App;
+declare(strict_types=1);
 
 $this->registerModule(
     'Mail to log',
     'Do not send mails but log them',
     'Jean-Christian Denis and contributors',
-    '0.6',
+    '0.7',
     [
         'requires'    => [['core', '2.28']],
-        'permissions' => App::auth()->makePermissions([
-            App::auth()::PERMISSION_USAGE,
-        ]),
+        'permissions' => 'My',
         'type'       => 'plugin',
         'support'    => 'https://git.dotclear.watch/JcDenis/' . basename(__DIR__) . '/issues',
         'details'    => 'https://git.dotclear.watch/JcDenis/' . basename(__DIR__) . '/src/branch/master/README.md',
---
+++ src/My.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dotclear\Plugin\mail2log;
+
+use Dotclear\Module\MyPlugin;
+
+/**
+ * @brief   mail2log My helper.
+ * @ingroup mail2log
+ *
+ * @author      Jean-Christian Denis (author)
+ * @copyright   GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
+class My extends MyPlugin
+{
+    // use default permissions
+}
